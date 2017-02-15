5f2092f96b1419356b2f93f453106b74f4da2c44
Ovidiu Mitroi <mitroio@coventry.ac.uk>
2017-02-15 10:46:27 +0000

    Added ClassDiagram and Code

---
+++ Documentation/Code/Ovidiu-AddSession/OvidiuAddSession.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ovidiu.addsession;
+
+/**
+ *
+ * @author mitroio
+ */
+public class OvidiuAddSession {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
