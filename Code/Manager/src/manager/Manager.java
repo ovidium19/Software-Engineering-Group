9d4d9c4a5a8ddcece43e2431053df3311ee7b3ef
Genaro Bedenko <genarobedenko@hotmail.com>
2017-02-23 15:28:22 +0000

    Project files for Manager uploaded

---
+++ Code/Manager/src/manager/Manager.java
@@ -0,0 +1,21 @@
+package manager;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/*
+ * Class that contains the main function
+ * Creates an instance of the ManagerUI
+ *
+ * @author Genaro Bedenko
+ */
+public class Manager {
+
+    public static void main(String[] args){
+
+      ManagerUI managerInterface = new ManagerUI();  
+      managerInterface.showEventDemo();
+   } 
+    
+}
