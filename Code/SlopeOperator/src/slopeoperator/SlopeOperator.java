c0f5300f0d6865148ac6ae48b907759db3fc8e65
Genaro Bedenko <genarobedenko@hotmail.com>
2017-02-23 15:07:08 +0000

    Project files for Slope Operator uploaded

---
+++ Code/SlopeOperator/src/slopeoperator/SlopeOperator.java
@@ -0,0 +1,21 @@
+package slopeoperator;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/*
+ * Class that contains the main function
+ * Creates an instance of the SlopeOperatorUI
+ *
+ * @author Genaro Bedenko
+ */
+public class SlopeOperator {
+
+    public static void main(String[] args){
+
+      SlopeOperatorUI slopeOperatorInterface = new SlopeOperatorUI();  
+      slopeOperatorInterface.showEventDemo();
+   } 
+    
+}
