e41c88dcca5f23a496c14594dce76b671c20adf6
Genaro Bedenko <genarobedenko@hotmail.com>
2017-02-23 15:18:29 +0000

    Project files for Ski Instructor uploaded

---
+++ Code/SkiInstructor/src/skiinstructor/SkiInstructor.java
@@ -0,0 +1,21 @@
+package skiinstructor;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/*
+ * Class that contains the main function
+ * Creates an instance of the SkiInstructorUI
+ *
+ * @author Genaro Bedenko
+ */
+public class SkiInstructor {
+
+    public static void main(String[] args){
+
+      SkiInstructorUI skiInstructorInterface = new SkiInstructorUI();  
+      skiInstructorInterface.showEventDemo();
+   } 
+    
+}
