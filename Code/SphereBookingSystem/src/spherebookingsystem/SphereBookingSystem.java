d692c4986a957d7ea78899f9c1da6b7fd4b41bed
Genaro Bedenko <genarobedenko@hotmail.com>
2017-03-14 15:03:17 +0000

    Created Welcome Screen JavaFX

--- Code/SphereBookingSystem/src/spherebookingsystem/SphereBookingSystem.java
+++ Code/SphereBookingSystem/src/spherebookingsystem/SphereBookingSystem.java
@@ -5,12 +5,22 @@
  */
 package spherebookingsystem;
 
+import java.util.HashSet;
+import java.util.Set;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 /**
@@ -21,9 +31,32 @@ public class SphereBookingSystem extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+             
+        Label welcomeText = new Label();
+        welcomeText.setText("Welcome to Sphere Booking & Checking In System");
+        welcomeText.setAlignment(Pos.TOP_CENTER);
+        welcomeText.setTextAlignment(TextAlignment.CENTER);
+        welcomeText.setPadding(new Insets(12,5,20,5));
+        
+        Label usernameLabel = new Label();
+        usernameLabel.setText("Enter Username...");
+        
+        TextField usernameText = new TextField();
+        usernameText.setAlignment(Pos.TOP_CENTER);
+        usernameText.setPrefSize(250, 30);
+        usernameText.setMaxSize(usernameText.getPrefWidth(), usernameText.getPrefHeight());
+        
+        Label passwordLabel = new Label();
+        passwordLabel.setText("Enter Password...");
+        
+        PasswordField passwordText = new PasswordField();
+        passwordText.setAlignment(Pos.TOP_CENTER);
+        passwordText.setPrefSize(250, 30);
+        passwordText.setMaxSize(passwordText.getPrefWidth(), passwordText.getPrefHeight());
+        
+        Button submitButton = new Button();
+        submitButton.setText("Submit");
+        submitButton.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
@@ -31,12 +64,25 @@ public class SphereBookingSystem extends Application {
             }
         });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        VBox userInfo = new VBox();
+        userInfo.getChildren().addAll(usernameLabel, usernameText);
+        userInfo.setAlignment(Pos.TOP_CENTER);
         
-        Scene WelcomeScene = new Scene(root, 300, 250);
+        VBox passwordInfo = new VBox();
+        passwordInfo.getChildren().addAll(passwordLabel, passwordText);
+        passwordInfo.setAlignment(Pos.TOP_CENTER);
         
-        primaryStage.setTitle("Hello World!");
+        VBox root = new VBox();
+        root.getChildren().addAll(welcomeText, userInfo, passwordInfo, submitButton);
+        root.setPadding(new Insets(50,50,50,50));
+        root.setAlignment(Pos.TOP_CENTER);
+        root.setSpacing(25);
+        
+                
+        
+        Scene WelcomeScene = new Scene(root, 500, 500);
+        
+        primaryStage.setTitle("Sphere Booking & Checking In System - Provided by InvoTech");
         primaryStage.setScene(WelcomeScene);
         primaryStage.show();
     }
